e05f3572fedaf58404164dff205a0fa9492d7f7d
choquitofrito <zyriab@gmail.com>
2022-03-29 14:17:03 +0200

    procfile

--- migrations/Version20220329115817.php
+++ migrations/Version20220329115817.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220323204122 extends AbstractMigration
+final class Version20220329115817 extends AbstractMigration
 {
     public function getDescription(): string
     {
